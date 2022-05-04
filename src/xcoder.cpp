6e1dc9c19ed704e0abebd80263e0ee38151dd2b8
hajix <sergioajini@gmail.com>
2022-05-04 13:41:53 +0430

    Fix bugs

--- src/xcoder.cpp
+++ src/xcoder.cpp
@@ -2,12 +2,11 @@
 
 
 map<string, int> Xcoder::create_symbols2index(const string graphemes_file) {
-    std::ifstream infile(graphemes_file);
-    std::string line;
+    ifstream infile(graphemes_file);
+    string line;
     int index = 0;
     map<string, int> result;
-    while (std::getline(infile, line)) {
-        std::istringstream iss(line);
+    while (getline(infile, line)) {
         result[line] = index;
         index++;
     }
@@ -16,12 +15,11 @@ map<string, int> Xcoder::create_symbols2index(const string graphemes_file) {
 
 
 map<int, string> Xcoder::create_index2symbols(const string graphemes_file) {
-    std::ifstream infile(graphemes_file);
-    std::string line;
+    ifstream infile(graphemes_file);
+    string line;
     int index = 0;
     map<int, string> result;
-    while (std::getline(infile, line)) {
-        std::istringstream iss(line);
+    while (getline(infile, line)) {
         result[index] = line;
         index++;
     }
@@ -31,4 +29,5 @@ map<int, string> Xcoder::create_index2symbols(const string graphemes_file) {
 
 bool Xcoder::load_model(const string model_path) {
     model = torch::jit::load(model_path);
+    return 1;
 }
--- src/xcoder.h
+++ src/xcoder.h
@@ -1,9 +1,12 @@
 #ifndef __TIHU__XCODER_H
 #define __TIHU__XCODER_H
 
+#include <map>
+#include <fstream>
 #include <torch/script.h>
 
 using namespace std;
+
 
 class Xcoder {
     torch::jit::script::Module model;
@@ -12,6 +15,6 @@ public:
     bool load_model(const string);
     map<string, int> create_symbols2index(const string);
     map<int, string> create_index2symbols(const string);
-}
+};
 
 #endif
